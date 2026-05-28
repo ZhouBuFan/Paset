43297d7adea0ee402dad591632722e95229bcb40
zhouhuinan <v_zhouhuinan@baidu.com>
2026-05-28 17:59:25 +0800

    fix: improve window show/hide behavior

--- paset.py
+++ paset.py
@@ -288,9 +288,9 @@ class PasetWindow(QWidget):
             self.show_and_activate()
     
     def show_and_activate(self):
-        if not self.isVisible():
-            self.setVisible(True)
-        self.show()
+        if self.isHidden():
+            self.setHidden(False)
+        self.showNormal()
         self.raise_()
         self.activateWindow()
     
